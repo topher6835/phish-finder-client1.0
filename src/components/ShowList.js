e1213a99e3530a552c28455eb338ebb6ad256fe4
Topher <sears.topher@gmail.com>
2019-04-14 22:42:20 -0400

    add checkbox check

--- src/components/ShowList.js
+++ src/components/ShowList.js
@@ -8,24 +8,13 @@ class ShowList extends Component {
 
     componentDidMount() {
         console.log("showList didmount");
-        // if(this.props.show) {
-        //     console.log(this.props.show);
-        // }
+
         if(this.props.year) {
             if(this.props.shows) {
                 if (this.props.shows.filter(e => e.yearId === this.props.year).length === 0) {
                     console.log("year not in state");
                     this.props.fetchShowsInYear(this.props.year);
                 }
-
-                // this.props.shows.map((show) => {
-                //     console.log(show);
-                    
-                //     if(show.yearId === (this.props.year) ) {
-                //         console.log("year in state.");
-                //         return;
-                //     }
-                // });
             }
             
             //this.props.fetchShowsInYear(this.props.year);
@@ -36,13 +25,27 @@ class ShowList extends Component {
         this.props.selectShow(showId);
     }
 
+    showSelectedCheck(id) {
+        if(this.props.selectedShows.includes(id)) {
+            return true;
+        }
+        return false;
+    }
+
     renderList() {
         if(this.props.shows) {
             return this.props.shows.map((show) => {
                 if(show.yearId === this.props.year) {
                     return (
                         <div key={show.id}>
-                            <p>{show.date} {show.venueName} {show.location} <Checkbox value={show.id} onChange={(e) => this.onChange(e, show.id)}></Checkbox> </p>
+                            <p>
+                                {show.date} {show.venueName} {show.location} 
+                                <Checkbox 
+                                    value={show.id} onChange={(e) => this.onChange(e, show.id)} 
+                                    checked={this.showSelectedCheck(show.id)}
+                                >
+                                </Checkbox> 
+                            </p>
                         </div>
                     );
                 }
