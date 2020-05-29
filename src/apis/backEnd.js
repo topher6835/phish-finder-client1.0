0c1bbaba5e1bce465a231ff00fae3bacbe87dc90
Chris <sears.topher@gmail.com>
2020-05-29 15:26:22 -0400

    update api url to digitalocean from aws

--- src/apis/backEnd.js
+++ src/apis/backEnd.js
@@ -1,11 +1,15 @@
 import axios from 'axios';
 
-const apiUrl = process.env.NODE_ENV === 'production' ? 'http://phishfinderservice-env.3uquabg3mf.us-east-2.elasticbeanstalk.com/api/' : '/api';
+const apiUrl = process.env.NODE_ENV === 'production' ? 'http://134.209.168.101:5000/api/' : '/api';
 
 export default axios.create({
     baseURL: apiUrl,
     timeout: 7000
 });
 
-// prod url 4/26/19
+// prod url 4/26/19 (aws)
 // http://phishfinderservice-env.3uquabg3mf.us-east-2.elasticbeanstalk.com/api/
+
+// prod url 5/29/20 (digitalocean)
+// *AWS free expired
+// http://134.209.168.101:5000/api/
